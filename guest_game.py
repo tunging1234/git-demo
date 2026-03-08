d15c29eb69b6185f358d73db7afb4ccaabc2ab3b
cathy <cathy@gmail.com>
2026-03-08 12:00:33 +0800

    增加猜數字的區間變數

--- guest_game.py
+++ guest_game.py
@@ -7,13 +7,15 @@ Created on Mon Jan 26 20:08:05 2026
 
 import random
 
+start,end = 1, 100
+
 # 產生亂數
-x=random.randint(1, 100)    
+x=random.randint(start, end)    
 # 提示答案
 #print(x)
 
 for i in range(10):
-    y=int(input("請猜一個數字(1~100):"))
+    y=int(input(f"請猜一個數字({start}~{end}):"))
     
     # 猜對 + break
     if x==y:
